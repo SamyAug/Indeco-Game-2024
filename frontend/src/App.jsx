dc9b7850618339c5de0d486cf53a9a55863eece2
Samuel Augustin <samuel.augustin@indecosoft.ro>
2024-07-02 13:13:04 +0300

    Game Board Added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+
+import GameBoard from "./GameBoard";
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-md-center">
+        <div className="col-6">
+          <GameBoard />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
