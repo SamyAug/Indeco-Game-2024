cf67c9ac76c87f2bc6ee7d6bfb5c5fd5442e3508
Alex Mascovici <adrianmascovici@yahoo.com>
2024-07-11 14:13:51 +0300

    player status "playing"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,27 @@
-import React, { useState } from 'react'
-import Register from './Register'
-import SocketContextProvider from './SocketContextProvider'
-import PlayerList from './PlayerList'
-import Game from './Game'
+import React, { useState } from "react";
+import Register from "./Register";
+import SocketContextProvider from "./SocketContextProvider";
+import PlayerList from "./PlayerList";
+import Game from "./Game";
 
 const App = () => {
-  const [userData, setUserData] = useState({})
-  const [games, setGames] = useState([])
+  const [userData, setUserData] = useState({});
+  const [games, setGames] = useState([]);
 
   return (
-      <SocketContextProvider>
-        {
-          Object.keys(userData).length
-          ?
-          <>
-            <PlayerList userData={userData} setGames={setGames}/>
-            {games.map((game) => <Game />)}
-          </>
-          :
-          <Register setUserData={setUserData}/>
-        }
-      </SocketContextProvider>
-  )
-}
+    <SocketContextProvider>
+      {Object.keys(userData).length ? (
+        <>
+          <PlayerList userData={userData} setGames={setGames} />
+          {games.map((game) => (
+            <Game key={game.userId} />
+          ))}
+        </>
+      ) : (
+        <Register setUserData={setUserData} />
+      )}
+    </SocketContextProvider>
+  );
+};
 
-export default App
+export default App;
