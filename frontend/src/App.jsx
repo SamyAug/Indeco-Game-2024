0f3618cc15fc281490aac4fb665b8405d6f713d0
Alex Mascovici <adrianmascovici@yahoo.com>
2024-07-16 11:42:32 +0300

    inceput de singleplayer ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import SocketContextProvider from "./SocketContextProvider";
 import PlayerList from "./PlayerList";
 import Game from "./Game";
 import Modal from "./Modal";
+import SingleplayerGameBoard from "./SingleplayerGameBoard";
 
 export const UserContext = createContext();
 
@@ -11,31 +12,38 @@ const App = () => {
   const [userData, setUserData] = useState({});
   const [games, setGames] = useState([]);
   const [userRelations, setUserRelations] = useState([]);
+  const [gamemode, setGamemode] = useState('singleplayer');
 
-  return (
-    <SocketContextProvider>
-      <UserContext.Provider value={{ userData, setUserData }}>
-        {Object.keys(userData).length ? (
-          <>
-            <PlayerList
-              setGames={setGames}
-              userRelations={userRelations}
-              setUserRelations={setUserRelations}
-            />
-            {games.map((game) => (
-              <Game
-                key={game.userId}
-                gameData={game}
+  return gamemode ? (
+    gamemode === "singleplayer" ? (
+      <Game />
+    ) : (
+      <SocketContextProvider>
+        <UserContext.Provider value={{ userData, setUserData }}>
+          {Object.keys(userData).length ? (
+            <>
+              <PlayerList
                 setGames={setGames}
+                userRelations={userRelations}
                 setUserRelations={setUserRelations}
               />
-            ))}
-          </>
-        ) : (
-          <Register />
-        )}
-      </UserContext.Provider>
-    </SocketContextProvider>
+              {games.map((game) => (
+                <Game
+                  key={game.userId}
+                  gameData={game}
+                  setGames={setGames}
+                  setUserRelations={setUserRelations}
+                />
+              ))}
+            </>
+          ) : (
+            <Register />
+          )}
+        </UserContext.Provider>
+      </SocketContextProvider>
+    )
+  ) : (
+    <SelectGameMode />
   );
 };
 
