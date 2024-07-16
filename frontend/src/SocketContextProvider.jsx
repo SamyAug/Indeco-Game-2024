c166073189cbc30bff82504b195cd97e4a2abe5a
Alex Mascovici <adrianmascovici@yahoo.com>
2024-07-16 12:10:17 +0300

    --

--- frontend/src/SocketContextProvider.jsx
+++ frontend/src/SocketContextProvider.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import { SocketContext } from "./SocketContext";
-const socketUrl = `https://game.sigma.team/`;
+const socketUrl = `ws://localhost:8080/`;
 //https://game.sigma.team/
 export default function SocketContextProvider({ children }) {
   //const [socket] = useState(new WebSocket(`ws://${location.host}`))
